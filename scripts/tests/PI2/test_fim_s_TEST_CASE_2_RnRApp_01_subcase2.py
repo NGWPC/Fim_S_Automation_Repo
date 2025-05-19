4aff2d5575f5f77515af813423a054dc864ed6f1
Parallel Works app-run user <jyoti.mikkilineni@mgmt-jyotimikkilineni-fimsnewclusteroejyoticopy-00015.optimizationuseast1-5.pw.local>
2025-05-19 17:05:54 +0000

    Adding PI-2 _ subcase 2 updates

--- scripts/tests/PI2/test_fim_s_TEST_CASE_2_RnRApp_01_subcase2.py
+++ scripts/tests/PI2/test_fim_s_TEST_CASE_2_RnRApp_01_subcase2.py
@@ -4,7 +4,7 @@ import os
 from ..helpers import validate_directories_files
 from ..utils import csv_util
 
-@pytest.mark.skip(reason="skipping this test for now")
+#@pytest.mark.skip(reason="skipping this test for now")
 def test_fim_s_TEST_CASE_2_RnRApp_01_subcase2(test_name,load_scenario_data,scenarios,fetch_docker_details,run_docker_script):
     tn = test_name + '.json'
     folder_name = 'PI2/data'
