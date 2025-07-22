36ef70d939cf099e22b0c71b09d0231cdd79fc10
Parallel Works app-run user <jyoti.mikkilineni@mgmt-jyotimikkilineni-fimsclusterautopi4-00009.optimizationuseast1-5.pw.local>
2025-07-22 21:25:59 +0000

    Added subcase 2b code

--- scripts/tests/helpers.py
+++ scripts/tests/helpers.py
@@ -79,7 +79,33 @@ def validate_get_response(link,headers,request_type):
    #    logging.error("source_response and destination_response responses do not match")
    #    raise e
    
-    
+def validate_post_response(link,headers,data,destination_response_location):
+    try:
+      source_response = None
+      destination_response = None
+      response = None
+      response = requests.post(link,headers=headers , data =json.dumps(data))
+      try:
+         response.raise_for_status()
+         if response.status_code == 200:
+            source_response = response.json()
+         return source_response
+      except requests.exceptions.HTTPError as errh:
+         print(f"Bad response: {response.status_code}")
+         raise errh
+      except requests.exceptions.ConnectionError as errc:
+         print("Conection error: " ,errc)
+         raise errc 
+      with open(destination_response_location, "r") as file:
+         destination_response = json.load(file)
+      # diff = jsondiff.diff(source_response,destination_response)
+      # with open('diff_response.txt','w') as diff_file:
+      #    diff_file.write(str(diff))
+      assert source_response == destination_response , "source_response and destination_response rdoes not esponses match"
+      logging.info("source_response and destination_response responses match")
+    except AssertionError as e:
+      logging.error("source_response and destination_response responses do not match")
+      raise e
    
 def hash_image_base64(b64):
    try:
